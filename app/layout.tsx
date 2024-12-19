b84e07ef50b186899e565e8a6286eaa02b81a38a
Dark <491298200@qq.com>
2024-12-19 19:31:31 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,10 +10,10 @@ import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
+  title: "WeGPT",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
-    title: "NextChat",
+    title: "WeGPT",
     statusBarStyle: "default",
   },
 };
